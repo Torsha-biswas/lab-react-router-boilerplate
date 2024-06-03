0ef5e1f5073406f0c87415dcf223d3d5e5dae432
Torsha-biswas <torshabiswas19@gmail.com>
2024-06-03 22:14:16 +0530

    done

---
+++ routing/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Navbar from './components/Navbar';
+import './App.css'; 
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
